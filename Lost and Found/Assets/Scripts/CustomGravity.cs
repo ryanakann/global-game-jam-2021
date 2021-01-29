019fd9e0073134142b8cc0df37e7c372590d8fee
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2021-01-29 14:43:14 +0000

    I'm GOIN through CHANGES

---
+++ Lost and Found/Assets/Scripts/CustomGravity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGravity : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public Vector3 gravity = new Vector3(0, -9.8f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //if some color condition:
+        rb.AddForce(gravity);
+    }
+}
